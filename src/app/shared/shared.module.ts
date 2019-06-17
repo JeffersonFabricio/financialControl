265e4a6608c95379a951fb674ca10fc96f0ec342
unknown <jeffersonfabricioma@gmail.com>
2019-06-16 22:37:42 -0300

    adding PageHeaderComponent to Pages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,23 +4,28 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from "@angular/forms";
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [
-    BreadCrumbComponent
-  ],
+  
   imports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule
   ],
+  declarations: [
+    BreadCrumbComponent,
+    PageHeaderComponent
+  ],
   exports: [
     // shared modules
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
 
-    //shared components
-    BreadCrumbComponent
+    // shared components
+    BreadCrumbComponent,
+    PageHeaderComponent
   ]
 })
 export class SharedModule { }
